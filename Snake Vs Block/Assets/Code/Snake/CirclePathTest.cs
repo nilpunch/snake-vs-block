2a8c55816b8997e65f9ec4cc47300a6a67cdaef3
Daniil Pankevich <dp@highcore.io>
2021-08-08 12:40:48 +0300

    Perfectly arranging circles on the path

---
+++ Snake Vs Block/Assets/Code/Snake/CirclePathTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Extensions;
+using UnityEngine;
+
+namespace Snake
+{
+    public class CirclePathTest : MonoBehaviour
+    {
+        [SerializeField] private int _pathSegments = 10;
+        [SerializeField] private int _snakeCircles = 10;
+        [SerializeField] private float _circlesRadius = 0.2f;
+        [SerializeField] private float _updateDistance = 0.1f;
+
+        private PathCirclesArranger _pathCirclesArranger;
+
+        private Vector2 _lastHeadPosition;
+
+        private void Awake()
+        {
+            _pathCirclesArranger = new PathCirclesArranger(_pathSegments, _circlesRadius);
+        }
+
+        private void Update()
+        {
+            Vector2 mousePosition = Input.mousePosition;
+
+            Plane raycastPlane = new Plane(Vector3.back, 0f);
+            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+
+            if (raycastPlane.Raycast(ray, out float distance))
+            {
+                Vector3 worldHitPoint = ray.GetPoint(distance);
+
+                if (Vector3.Distance(_lastHeadPosition, worldHitPoint) > _updateDistance)
+                {
+                    _pathCirclesArranger.AddPoint(worldHitPoint);
+                    _lastHeadPosition = worldHitPoint;
+                }
+                else
+                {
+                    _pathCirclesArranger.UpdateFirst(worldHitPoint);
+                }
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.white;
+
+            if (_pathCirclesArranger == null)
+                return;
+
+            if (_pathCirclesArranger.Count > 0)
+            {
+                foreach (var position in _pathCirclesArranger.Evaluate(_snakeCircles))
+                {
+                    Gizmos.DrawWireSphere(position, _circlesRadius);
+                }
+            }
+        }
+    }
+}
---
+++ Snake Vs Block/Assets/Code/Snake/PathCirclesArranger.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Snake
+{
+    public class PathCirclesArranger
+    {
+        private readonly Path _path;
+        private readonly float _circleRadius;
+
+        public PathCirclesArranger(int segmentsAmount, float circleRadius)
+        {
+            _circleRadius = circleRadius;
+            _path = new Path(segmentsAmount);
+        }
+
+        public int Count => _path.Points.Count;
+
+        public void AddPoint(Vector2 point)
+        {
+            _path.AddPoint(point);
+        }
+
+        public void UpdateFirst(Vector2 position)
+        {
+            _path.UpdateFirst(position);
+        }
+
+        public IEnumerable<Vector2> Evaluate(int circlesAmount)
+        {
+            if (circlesAmount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(circlesAmount));
+
+            if (_path.Points.Count == 0)
+                throw new InvalidOperationException();
+
+            if (_path.Points.Count == 1)
+            {
+                yield return _path.Points.Last();
+                yield break;
+            }
+
+            var reversedPath = _path.Points.Reverse().GetEnumerator();
+            reversedPath.MoveNext();
+            
+            Vector2 lastCirclePosition = reversedPath.Current;
+            yield return lastCirclePosition;
+
+            int circlesEvaluated = 1;
+            Vector2 lastPoint = lastCirclePosition;
+
+            while (circlesEvaluated != circlesAmount)
+            {
+                Vector2 point = reversedPath.Current;
+                
+                float distanceFromLastCircle = Vector2.Distance(point, lastCirclePosition);
+
+                if (distanceFromLastCircle < _circleRadius * 2f)
+                {
+                    lastPoint = point;
+                    if (reversedPath.MoveNext() == false)
+                        break;
+                    continue;
+                }
+
+                Vector2 newCirclePosition = CalculateOtherCircleCenter(lastPoint, point, lastCirclePosition);
+
+                yield return newCirclePosition;
+                circlesEvaluated += 1;
+                lastCirclePosition = newCirclePosition;
+            }
+            
+            while (circlesEvaluated != circlesAmount)
+            {
+                yield return lastPoint;
+                circlesEvaluated += 1;
+            }
+        }
+
+        private Vector2 CalculateOtherCircleCenter(Vector2 start, Vector2 end, Vector2 circleCenter)
+        {
+            Vector2 direction = end - start;
+            Vector2 directionToTangent = Vector2.Dot(circleCenter - start, direction)
+                / direction.sqrMagnitude * direction;
+            Vector2 tangentPoint = start + directionToTangent;
+            float distanceToLine = ((circleCenter - start) - directionToTangent).magnitude;
+            float hypotenuse = _circleRadius * 2f;
+
+            float otherTriangleLength = Mathf.Sqrt(hypotenuse * hypotenuse - distanceToLine * distanceToLine);
+
+            return tangentPoint + direction.normalized * otherTriangleLength;
+        }
+    }
+}
